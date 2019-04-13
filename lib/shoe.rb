b83a0ff1ed49e72b7487c570d13c2763cb7e2b6a
Christopher Luna <chris@illusionlayervfx.com>
2019-04-13 01:44:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,15 @@ require "pry"
 
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  
 
   BRANDS = []
 
   def initialize(brand)
   @brand = brand
   end
+
+
 
   def cobble
     self.condition = "new"
